1508499ae5ee10c6752a14994594401e31d22000
Juyalashutosh51-code <Juyalashutosh51@gmail.com>
2026-09-14 19:21:32 +0530

    added analytics

--- app/main.py
+++ app/main.py
@@ -3,12 +3,14 @@ from fastapi import FastAPI
 from app.database import Base, engine
 from app.models.transaction import Transaction
 from app.routers.transaction import router as transaction_router
+from app.routers.analytics import router as analytics_router
 
 Base.metadata.create_all(bind=engine)
 
 app=FastAPI()
 
 app.include_router(transaction_router) 
+app.include_router(analytics_router)
 
 @app.get("/")
 def home():
